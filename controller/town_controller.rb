561f3649586d033bd00dda526fa5a22f90fe19bc
sirtau <contact@hamidmousa.com>
2021-06-19 14:52:19 +1000

    Revert "Update town_controller.rb"
    This reverts commit 035abb409ab56a79708437d00dbb76386a787328.

--- controller/town_controller.rb
+++ controller/town_controller.rb
@@ -1,6 +1,6 @@
 get '/town' do
-    if session[:character_hash]
     character_hash = session[:character_hash]
+
+
     erb :'/game/game', locals: { character_hash: character_hash }
-    else redirect '/'
 end
